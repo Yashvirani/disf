e02b5addf931632d69fa154c41fa512c6667f28c
Yashvirani <viraniyash2001@gmail.com>
2020-10-20 17:04:30 +0530

    commit

--- src/components/ChatHeader/ChatHeader.js
+++ src/components/ChatHeader/ChatHeader.js
@@ -3,6 +3,9 @@ import './ChatHeader.css';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
 import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
 
 function ChatHeader() {
     return (
@@ -19,10 +22,14 @@ function ChatHeader() {
                 <NotificationsIcon />
                 <EditLocationRoundedIcon />
                 <PeopleAltRoundedIcon />
+                <div className="chatHeader__search">
+                   <input placeholder="Search"/>
+                   <SearchRoundedIcon />
+                </div>
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
             </div>
-            <div className="chatHeader__search">
-                <input placeholder="Search"/>
-            </div>
+           
         </div>
     )
 }
