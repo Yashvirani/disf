f7f1ed78a791a5071412de8649a2f117dda928ba
Yashvirani <viraniyash2001@gmail.com>
2020-10-20 15:25:42 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />  
+    </div>
+  );
+}
+
+export default App;
